4c146a5b0d72cd02d2603dabd65f304d1da6023b
Cesar_Cervantes <ces_29_1995@hotmail.com>
2021-03-11 22:11:07 -0600

    Add logic to change states values

--- client/src/components/Contacto.js
+++ client/src/components/Contacto.js
@@ -5,10 +5,18 @@ function Contacto() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [message, setMessage] = useState('');
+    
     return (
         <section id="contacto">
             <h2>Contacto</h2>
-            <Form></Form>
+            <Form 
+                changeName={setName}
+                changeEmail={setEmail}
+                changeMessage={setMessage}
+                name={name}
+                email={email}
+                message={message}
+            ></Form>
         </section>
     );
 };
--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Button from './Button';
 import '../styles/Form.css';
-function Form() {
+function Form({ changeName, changeEmail, changeMessage, name, email, message}) {
     return (
         <form>
-            <input type="text" placeholder="Escribe tu nombre"/>
-            <input type="text" placeholder="Escribe tu email"/>
-            <textarea placeholder="Escribe tu mensaje"/>
+            <input 
+                type="text" 
+                placeholder="Escribe tu nombre"
+                onChange={ ({target}) => changeName(target.value)}    
+                value={name}
+                />
+            <input 
+                type="email" 
+                placeholder="Escribe tu email"
+                onChange={({target}) => changeEmail(target.value)}    
+                value={email}
+                />
+            <textarea 
+                placeholder="Escribe tu mensaje"
+                onChange={({target}) => changeMessage(target.value)}
+                value={message}
+                />
             <Button type="submit">Enviar</Button>
         </form>
     );
