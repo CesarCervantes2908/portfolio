9a4ccfed4eb4bdfbbbe3a75531e2122b5185817b
Cesar_Cervantes <ces_29_1995@hotmail.com>
2021-03-12 13:10:21 -0600

    Change favicon

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT;
@@ -12,6 +12,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(morgan('dev'));
 app.use(router);
 
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
 
 
 app.listen(PORT, ()=>{
