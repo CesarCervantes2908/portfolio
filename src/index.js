427ee710ab2562e441f9f80bbb3e8c30b51fe716
Cesar_Cervantes <ces_29_1995@hotmail.com>
2021-03-12 15:06:46 -0600

    Create conditionals for production enviroment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 
+if(process.env.NODE_ENV === 'development'){
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
 const app = express();
 const PORT = process.env.PORT;
 const router = require('./routes/router');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(morgan('dev'));
 app.use(router);
 
 
