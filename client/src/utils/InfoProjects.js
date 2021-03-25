ca3f01ac91757a14d7ed3f084a87e306ff77a09e
Cesar_Cervantes <ces_29_1995@hotmail.com>
2021-03-24 19:31:57 -0600

    Add BurritosApp

--- client/src/utils/InfoProjects.js
+++ client/src/utils/InfoProjects.js
@@ -19,7 +19,8 @@ let projects = [
         images: [metaGatoMain, metaGato1, metaGato2, metaGato3],
         smallDesc: "Web que tiene el propósito de testear algoritmos de inteligencia artificial a través de un juego de meta gato",
         skills: ["HTML", "CSS", "JavaScript", "React", "Node", "Express"],
-        desc: "Esta web fue desarrollada para el Centro de Investigaciones en Computación del IPN. Específicamente para el laboratorio de inteligencia artificial. Tiene el propósito de evaluar el desempeño de algoritmos de IA mediante un juego de tic tac toe."
+        desc: "Esta web fue desarrollada para el Centro de Investigaciones en Computación del IPN. Específicamente para el laboratorio de inteligencia artificial. Tiene el propósito de evaluar el desempeño de algoritmos de IA mediante un juego de tic tac toe.",
+        link: 'https://meta-gato.herokuapp.com/'
     },{
         id: 'salamandramilk',
         name: "SalamandraMilk",
@@ -30,7 +31,19 @@ let projects = [
         images: [salamandraMain, salamandra1, salamandra2, salamandra3, salamandra4],
         smallDesc: "Web para promoción e información de un negocio de productos orgánicos",
         skills: ["HTML", "CSS", "JavaScript", "React", "React Router"],
-        desc: "Esta web fue diseñada y desarrollada para promover un pequeño negocio local dedicado a la producción y venta de productos orgánicos. El sitio web consta de 3 páginas en las que se puede encontrar información de la empresa y los beneficios de sus productos, el cátalogo de productos con sus precios y características y una forma de contactar con la empresa para poder realizar un pedido"
+        desc: "Esta web fue diseñada y desarrollada para promover un pequeño negocio local dedicado a la producción y venta de productos orgánicos. El sitio web consta de 3 páginas en las que se puede encontrar información de la empresa y los beneficios de sus productos, el cátalogo de productos con sus precios y características y una forma de contactar con la empresa para poder realizar un pedido",
+        link: 'https://salamandramilk.shop'
+    },{
+        id: 'burritosApp',
+        name: 'BurritosApp',
+        mainImg: {
+
+        },
+        images: [],
+        smallDesc: "Aplicación Web que permite administrar un negocio local de venta de burritos y tacos",
+        skills: ["HTML", "Bootstrap", "React", "React Router", "React Hooks", "Node.JS", "Express", "MongoDB"],
+        desc: "Esta apliación Web permite administrar un negocio. Está enfocada a un negocio de mi localidad de venta de burritos y tacos. Cuenta con tres apartados principales. Ventas; permite ver los días en los que se han tenido ventas, así como el total que se vendió en ese día. Así mismo, si das click en la fecha del día te llevará a una nueva página en la que podrás ver todos los clientes de ese día. Estos datos los podrás generar a través de la app, ya que también te permite ingresar nuevas cuentas para cada día, así como calcular el cambio que necesitas dar con base en el pago recibido. El segundo apartado es Gastos: el cuál te permite generar una lista de compras, la lista de compras actual aparecerá en la página principal del sitio. El tercer apartado es de pendientes, el cual te permite generar una lista de pendientes, igualmente la actual aparecerá en el inicio. Ambos apartados antes mencionados permiten ver listas generadas anterior mente y se organizan por fechas al igual que las ventas. Adicionalmente la app, registra todo el efectivo que entra al negocio a través de las ventas y el efectivo que sale a través de las compras. ¡Prueba la demo en el link de abajo! Y si la quieres para tu negocio o necesitas algo similar, ¡Contáctame!",
+        link: 'https://burritosapp.herokuapp.com/'
     }
 ];
 
